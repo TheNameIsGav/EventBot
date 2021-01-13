b63351d316ac0ab365901c72210d0df982d01cd3
GManners-Trinity <gmanners@trinity.edu>
2021-01-13 17:15:01 -0600

    gitignore and login stuff

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
+##"C:\Users\D6rks\AppData\Local\Programs\Python\Python37-32/python.exe" d:/Code/EventBot/bot.py
+
 import os
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 
-with open ("token.token", 'r') as tokenFile:
-    preToken = tokenFile[0]
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-TOKEN = os.getenv(preToken)
+client = discord.Client()
 
+@client.event
+async def on_read():
+    print(f'{client.user} has connected to Discord!')
 
+client.run(TOKEN)

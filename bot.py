e928ea90be0246ca6eb78b89a7f32c0c297a4495
GManners-Trinity <gmanners@trinity.edu>
2021-01-13 18:00:07 -0600

    added task timer functionality and defined task checker

--- bot.py
+++ bot.py
@@ -1,17 +1,52 @@
-##"C:\Users\D6rks\AppData\Local\Programs\Python\Python37-32/python.exe" d:/Code/EventBot/bot.py
+##python bot.py
 
 import os
+import datetime
 import discord
+import threading
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+currentTime = datetime.datetime.now()
+amOnline = False
+
+def taskTimer():
+    if(amOnline):
+        checkTasks()
+        #                           \/ Time in seconds between Checks
+        mytimer = threading.Timer(3600.0, taskTimer)
+        mytimer.start()
 
 @client.event
-async def on_read():
-    print(f'{client.user} has connected to Discord!')
+async def on_ready():
+    global amOnline
+    print('We have logged in as {0.user}'.format(client))
+    print('Setting current time as {0}'.format(currentTime))
+    amOnline = True
+
+    #starting timer
+    taskTimer()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$quit'):
+        global amOnline
+        amOnline = False
+        print("Shutting down")
+        await message.channel.send('Shutting down!')
+        exit(1)
+
+
 
 client.run(TOKEN)

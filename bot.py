5a5b26ef58d5f737d7e669ae7d60efe4aab66815
GManners-Trinity <gmanners@trinity.edu>
2021-01-26 16:25:59 -0600

    added some comments and some permission limits to quit commands

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ client = discord.Client()
 startTime = datetime.now()
 amOnline = False
 
-
+#Starts multithreaded timer to check tasks at the interval
 async def taskTimer():
     global amOnline
     loop = asyncio.get_running_loop()
@@ -27,12 +27,13 @@ async def taskTimer():
         #todo await updateTasks (remove tasks that have been completed)
         if(not amOnline):
             break
-        await asyncio.sleep(15)
+        await asyncio.sleep(900) #number of seconds in the interval to check tasks
+        #currently set to 15 minutes
 
 async def checkTasks():
-    minThresh = 30
-    currentTime = datetime.now(tz=timezone.utc).timestamp()
+    minThresh = 30 #minutes in advance to check for tasks
 
+    currentTime = datetime.now(tz=timezone.utc).timestamp()
     lines = []
 
     with open ('tasks.csv', 'r') as file:
@@ -57,18 +58,19 @@ async def checkTasks():
                     await user.send("You have a meeting at {0}, which is in {1} minutes".format(realTime, mins))
                 else:
                     await user.send("You have a meeting at {0}, which is in about {1} minute(s)\n\nYou gave the description as:\n {2}".format(realTime, math.floor(mins), description))
-        #Checks to see if the task is in the past, and marks for deletiong                            
+
+        #Checks to see if the task is in the past, and if so deletes it from the lines                          
         if mins < 0:
             del lines[lines.index(line)]
     
+    #rewrites the file without tasks in the past
     with open ('tasks.csv', 'w') as file:
         for line in lines:
             file.write(line)
     
-    #Handles deleting marked events during a check task
-    
     return
 
+#converts time from military time to standard time
 def timeConvert(miliTime):
     hours, minutes, seconds = str(miliTime).split(":")
     hours, minutes = int(hours), int(minutes)
@@ -78,6 +80,8 @@ def timeConvert(miliTime):
         hours -= 12
     return (("%02d:%02d " + setting) % (hours, minutes))
 
+#command called when a user executes the $addTask function
+#little to no error checking, so precise commands are a current nessecity
 def addTask(inputString, author):
     split = inputString.split('|')
     date = split[0]
@@ -108,38 +112,46 @@ def addTask(inputString, author):
         else:
             file.write('{0},{1},{2}\n'.format(writableDate, author, description))
     
-
+#On Ready event for Discord API. Starts the timer
 @client.event
 async def on_ready():
     global amOnline
     print('We have logged in as {0.user}'.format(client))
-    print('Setting current time as {0}'.format(startTime))
+    print('Current time is {0}'.format(startTime))
     amOnline = True
 
     #starting timer
     await taskTimer()
 
+#Sets up Discord API on_message event, which is called whenever the bot sees a message. 
 @client.event
 async def on_message(message):
+    #Prevents the bot from messaging itself
     if message.author == client.user:
         return
 
+    #Test function to assure teh bot works correct
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+    #Function to add task to the internal calender (csv file)
     if message.content.startswith('$addTask'):
         addTask(message.content[9:], message.author.id)
         await message.channel.send('Added task to internal calender')
 
+    #Sends a help command giving the format of the command
     if message.content.startswith('$help'):
         await message.channel.send('Task format is: \n $addTask Month, Day, Year | HH:MM (In Military) | Description (If applicable)')
 
-    if message.content.startswith('$quit'):
-        global amOnline
-        amOnline = False
-            
-        print("Shutting down")
-        await message.channel.send('Shutting down!')
-        exit(1)
+    #command that can be used to quit the bot in the case that it is misbehaving
+    #Can only be used by users with administrator privileges in the current channel.
+    if message.author.permissions_in(message.channel).administrator:
+        if message.content.startswith('$quit'):
+            global amOnline
+            amOnline = False
+                
+            print("Shutting down")
+            await message.channel.send('Shutting down!')
+            exit(1)
 
 client.run(TOKEN)
